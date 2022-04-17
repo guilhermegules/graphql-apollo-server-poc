cc5bf0a2586e9761a9604f95754418576497e765
Guilherme Gules Moreira <guilhermegules@gmail.com>
2022-04-17 19:06:22 -0300

    feat: creating date time custom scalar type

--- src/api/user/resolvers/userResolvers.js
+++ src/api/user/resolvers/userResolvers.js
@@ -1,11 +1,20 @@
+import { GraphQLScalarType } from 'graphql'
+
 export const userResolvers = {
+  DateTime: new GraphQLScalarType({
+    name: 'DateTime',
+    description: 'date string of date and hour in format ISO-8601',
+    serialize: (value) => value.toISOString(),
+    parseValue: (value) => new Date(value),
+    parseLiteral: (ast) => new Date(ast.value)
+  }),
   query: {
     users: (root, args, context, info) => context.dataSources.usersAPI.getUsers(),
     user: (root, { id }, context, info) => context.dataSources.usersAPI.getUserById(id),
   },
   mutation: {
-    addUser: (root, user, context, info) => context.dataSources.usersAPI.addUser(user),
-    updateUser: (root, user, context, info) => context.dataSources.usersAPI.updateUser(user),
+    addUser: (root, { user }, context, info) => context.dataSources.usersAPI.addUser(user),
+    updateUser: (root, args, context, info) => context.dataSources.usersAPI.updateUser(args),
     deleteUser: (root, { id }, context, info) => context.dataSources.usersAPI.deleteUser(id)
   }
 }
